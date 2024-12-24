46d8740371918402f42d9931111cba5f72f93e4d
Your Name <you@example.com>
2024-12-24 16:07:15 -0500

    33_ConcatenatetwoTuples.py

---
+++ 32_AccessAnElementInATuple.py
@@ -0,0 +1,11 @@
+# Write a program to access an element in a tuple. (It similar to a list but tuples are immutable)
+# Algorithm: 1_ Initialize a tuple with the desired elements. 2_Use the index to access a specific element in the tuple. 3_Print the accessed element.
+
+#Initialize a tuple
+my_tuple = [10, 20, 30, 40, 50]
+
+#Access the element at the index 2 ( index starts from 0)
+element = my_tuple[2]
+
+#Print the accessed element
+print("The element at index 2 in the tuples is:", element)
