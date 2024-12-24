bb82cd8412d9955ec5c6522ca5ae5de1ed9241fa
Your Name <you@example.com>
2024-12-24 16:54:54 -0500

    35_CovertATupleToAList.py

---
+++ 34_GetTheLengthOfATuple.py
@@ -0,0 +1,11 @@
+# Write a program to determine the length of a tuple.
+# Algorithm: 1_Initialize a tuple with the desired elements. 2_Use the len() function to determine the length of the tuple. 3_Print the length of the tuple.
+
+#Initialize a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+#Get the length of the tuple
+tuple_length = len(my_tuple)
+
+#Print the length of the tuple
+print("The length of the tuple is:", tuple_length)
