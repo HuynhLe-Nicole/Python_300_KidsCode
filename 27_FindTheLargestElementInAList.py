58278da5a148ff44b9b6f4b1d98a034ef2e74e8f
Your Name <you@example.com>
2024-12-24 14:42:01 -0500

    28_FindTheSmallestElementInAList.py

---
+++ 27_FindTheLargestElementInAList.py
@@ -0,0 +1,11 @@
+# Write a program to find the largest element in a list
+# Algorithm: 1_Initialize the initial list. 2_Use the max() function to find the largest element in the list. 3_Print the largest element.
+
+#Initialize the initial list 
+my_list = [34, 1, 23, 4, 3, 12, 45, 33, 6, 22]
+
+#Find the largest element in the list
+max_element = max(my_list)
+
+#Print the largest element 
+print("The largest element in the list is:", max_element)
