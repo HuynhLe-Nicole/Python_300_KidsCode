32b99e8b93f4b4a56418e5f89b236cc01eef0eb0
Your Name <you@example.com>
2024-12-24 16:15:26 -0500

    34_GetTheLengthOfATuple.py

---
+++ 33_ConcatenatetwoTuples.py
@@ -0,0 +1,12 @@
+# Write a program to concatenate two tuples.
+# Algorithm: 1_Initialize two tuples with the desired elements. 2_Use the + operator to concatenate the two tuples together. 3_Print the resulting tuple after concatenation.
+
+# Initialize two tuples
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+#Concatenate the two tuples together
+combined_tuple = tuple1 + tuple2
+
+#Print the resulting tuple
+print("Tuple after concatenation:", combined_tuple)
