6c9e7c6ae9ff26f2d3074c4685b30ebf59b94f72
Your Name <you@example.com>
2024-12-24 15:56:39 -0500

    32_AccessAnElementInATuple.py

---
+++ 31_CreateATuple.py
@@ -0,0 +1,8 @@
+# Write a program to create a tuple
+# Algorithm: 1_Initialize a tuple with desired elements. 2_Print the created tuple
+
+#Initialize a tuple
+my_tuple = [1, 2, 3, 4, 5]
+
+#Print the created tuple
+print("The created tuple:", my_tuple)
