3ecddbf4628331669762aa88c5d28143434d1d1c
Your Name <you@example.com>
2024-12-25 16:41:23 -0500

    52_AddAnElementToASet.py

---
+++ 51_CreateASet.py
@@ -0,0 +1,10 @@
+# Write a program to create a set using two different method: 1_Using curly braces {}. 2_Using the buil-in set() function.
+#Algorithm: 1_Use the curly braces {} to initialize the set. 2_Use the built-in set() function to initialize the set.
+
+#Method 1: Use curly braces {} to create a set
+set1 = {'apple', 'banana', 'cherry'}
+print("Set created using curly braces {}:", set1)
+
+#Method 2: Use the set() function to create a set
+set2 = set(['apple', 'banana', 'cherry'])
+print("Set created using the set() function:", set2)
