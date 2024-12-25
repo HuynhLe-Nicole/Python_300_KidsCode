fcd2ef033fa9f84147d4990b3117af282d2b5377
Your Name <you@example.com>
2024-12-24 19:18:40 -0500

    37_CheckIfAnElement existsInATuple.py

---
+++ 36_ConvertAListIntoATuple.py
@@ -0,0 +1,11 @@
+# Write a program to convert a list into a tuple.
+# Algorithm: 1_Initialize a list with the desired elements. 2_Use the tuple() function to convert the list into a tuple. 3_Print the resulting tuple.
+
+#Intialize a list
+my_list = [10, 20, 30, 40, 50]
+
+#Convert the list into a tuple
+my_tuple = tuple(my_list)
+
+#Print the converted tuple
+print("Tuple after conversion from list:", my_tuple)
