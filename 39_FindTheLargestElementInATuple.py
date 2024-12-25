58503e079e2fb0b878eb596636866a074a1a1a60
Your Name <you@example.com>
2024-12-24 20:56:49 -0500

    40_FindTheSmallestElementInATupleWithoutUsingTheMin()Function.py

---
+++ 39_FindTheLargestElementInATuple.py
@@ -0,0 +1,12 @@
+# Write a program that finds the largest element in a tuple.
+#Algorithm: 1_Initialize a tuple with the desired element. 2_Use the max() function to find the largest element in the tuple. 3_Print the largest element.
+
+#Initialize a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+#Find the largest element in the my_tuple
+max_element = max(my_tuple)
+
+#Print the largest element
+print("The largest element in the tuple is:", max_element)
+
