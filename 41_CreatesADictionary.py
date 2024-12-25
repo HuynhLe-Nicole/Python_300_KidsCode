e50b2f59bce2dbc8734de1cd3447d7bbc8dd1a95
Your Name <you@example.com>
2024-12-24 21:35:54 -0500

    42_AccessTheValueOfValueOfAKeyInADictionary.py

---
+++ 41_CreatesADictionary.py
@@ -0,0 +1,23 @@
+# Write a program tha creates a dictionary. A dictionary is a data structure that allows you to store key-value pairs. Each key in a dictionary is unique and maps to a specific value.
+# Algorithm: 1_Initialize an empty dictionary or one with initial key-value pairs. 2_(Optional) Add or update key-value pairs in the dictionary. 3_Print the created dictionary.
+
+#Initialize an empty dictionary
+my_dict = {}
+
+#Add key-value pairs to the dictionary
+my_dict['name'] = 'Alice'
+my_dict['age'] = 25
+my_dict['city'] = 'NewYork'
+
+#Print the created dictionary
+print("Created dictionary:", my_dict)
+
+#Initialize a dictionary with inital key-value pairs
+my_dict_2 = {
+    'name' : 'Bob',
+    'age' : 30,
+    'city' : 'Los Angeles'
+}
+
+#Print the created dictionary
+print("Created dictionary:", my_dict_2)
