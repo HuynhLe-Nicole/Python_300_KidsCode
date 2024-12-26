314c24e80bcd938fa169fde2338b7f64d8d2e1cf
Your Name <you@example.com>
2024-12-25 19:31:47 -0500

    56_FindTheUnionOfTwoSets.py

---
+++ 55_GetTheLengthOfASet.py
@@ -0,0 +1,10 @@
+# Write a program to obtian the length of a set 
+# Algorithm: 1_Initialize an initial set. 2_Get the length of the set using the len() function.
+
+#Initialize an initial set
+my_set = {'apple', 'banana', 'cherry'}
+print("Initial set:", my_set)
+
+#Get the length of the set
+length = len(my_set)
+print("Length of the set:", length)
