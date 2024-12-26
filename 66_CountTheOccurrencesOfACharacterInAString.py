f075faf02470b493e79878926766a5520da07a43
Your Name <you@example.com>
2024-12-25 22:03:58 -0500

    67_CheckIfAStringContainASpecificWord.py

---
+++ 66_CountTheOccurrencesOfACharacterInAString.py
@@ -0,0 +1,10 @@
+# Write a program to count the occurrences of character in a string. Use the count() method of strings
+# Algorithm: 1_Input a string. 2_Input the character to count. 3_Use the count() method to count the occurrences of the character in the string. 4_Print the count result.
+
+#Input a string 
+input_str = "hello world"
+print("Original string:", input_str)
+
+#Input the character to count
+char_to_count = 'l'
+print("Character to count:", char_to_count)
