bee4c68e28e6e07281ba25e8bd72dd5a42c59493
Your Name <you@example.com>
2024-12-25 19:52:58 -0500

    58_FindTheDifferenceOfTwoSets.py

---
+++ 57_FindTheIntersectionOfTwoSets.py
@@ -0,0 +1,12 @@
+# Write a program to find the intersection of two sets 
+# Algorithm: 1_Use the two sets initialized from the previous code. 2_Find the intersection of the two sets using the & operator or the intersection() method
+
+# Initialize two initial sets
+set1 = {'apple', 'banana', 'cherry'}
+set2 = {'banana', 'cherry', 'durian'}
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+#Find the intersection of the two sets
+set_intersection = set1 & set2
+print("Intersection of the two sets:", set_intersection)
