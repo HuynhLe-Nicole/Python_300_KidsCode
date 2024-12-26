ee817fdbf172a9e80f08f1cf9e0903e13eeb2a69
Your Name <you@example.com>
2024-12-25 21:44:05 -0500

    65_ConvertAStringToLowercase.py

---
+++ 64_ConvertAStringToUppercase.py
@@ -0,0 +1,10 @@
+# Write a program that converts a string to uppercase.
+# Algorithm: 1_Input a string. 2_Use the upper()  method to convert the string to uppercase. 3_Print the converted string.
+
+#Input a string 
+input_str = "hello world"
+print("Original string:", input_str)
+
+#Convert the string to uppercase
+upper_str = input_str.upper()
+print("String after conversion to uppercase:". upper_str)
